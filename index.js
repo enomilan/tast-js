e82e943382a322cc7b7dc242f518207209c9b7d1
enomilan <andrewmusta@gmail.com>
2022-05-11 23:12:14 -0500

    js

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const Api = (() => {
       
       
     }).then((res) => res.json());
+
+    const getList = () => 
+    fetch("http://localhost:3000/todos")
+        .then((res) => res.json())
+        // .then((json) => console.log(json))
+
+    const deleteList = (id) =>
+        fetch(`http://localhost:3000/todos/${id}`, {
+        method: 'DELETE'
+    })
+
+    return {
+        getList, deleteList, addList,
+    }
+
+})()
